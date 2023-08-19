fa1da14031898a9e8e821c8a795122d3bc6db821
Abhishek Jain <abhishek.j0407@gmail.com>
2023-08-19 14:24:18 +0530

    Assets added, new assignment created

--- src/components/Main/Assessments/Assessments.jsx
+++ src/components/Main/Assessments/Assessments.jsx
@@ -1,3 +1,5 @@
+import "./Assessments.css";
+
 const assessments = [
   {
     img: "",
@@ -32,22 +34,29 @@ const Assessments = () => {
   return (
     <section className="assessments">
       <h3>My Assessment</h3>
-      <div className="content">
-        <div className="new-assessment">New</div>
+      <div className="assessments-content">
+        <div className="new-assessment">
+          <div className="add-icon">
+            <img src="/assets/menu/add.svg" alt="add" />
+          </div>
+          <h4>New Assessment</h4>
+          <p>
+            From here you can add questions of multiple types like MCQs,
+            subjective (text or paragraph)!
+          </p>
+        </div>
         {assessments.map((assessment, index) => (
           <div className="assessment" key={index}>
-            <img
-              src="/assets/menu/captive_portal.svg"
-              alt={assessment.title}
-              className="icon"
-            />
-            <div className="content">
+            <div className="assessment-icon">
+              <img src="/assets/menu/briefcase.svg" alt={assessment.title} />
+            </div>
+            <div className="assessment-main">
               <h4>{assessment.title}</h4>
               <div className="info">
                 <p className="purpose">{assessment.purpose}</p>
                 <div className="date">
-                  <img src="/assets/menu/captive_portal.svg" alt="date_icon" />
-                  <p className="date-text">{assessment.date}</p>
+                  <img src="/assets/menu/calendar_today.svg" alt="date_icon" />
+                  <p>{assessment.date}</p>
                 </div>
               </div>
               <div className="details">
@@ -64,7 +73,7 @@ const Assessments = () => {
                 <div className="share-links">
                   <a href="#" className="link">
                     <img
-                      src="/assets/menu/captive_portal.svg"
+                      src="/assets/menu/link.svg"
                       alt="share_icon"
                     />
                     <p>Share</p>
